5032a7a92c82115e74120fd14f6671fefda0184b
Chantel Yip <chantel.yip@banno.com>
2021-10-02 22:07:20 -0700

    use read

--- helloScript.sh
+++ helloScript.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
-MESSAGE="Hello LinuxHint Audience"
-export MESSAGE
-./secondScript.sh 
+echo "enter 1st string"
+read st1
 
+echo "enter 2nd string"
+read st2
+
+if [ "$st1" == "$st2" ]
+then 
+	echo "strings match"
+else
+	echo "strings don't match"
+fi
 
